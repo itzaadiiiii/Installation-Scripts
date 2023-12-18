74d35337b2540361bae1e1cc78aa227b15378a14
Aditya Patil <98164681+itzaadiiiii@users.noreply.github.com>
2023-12-18 09:24:12 +0530

    Create firstscript.sh
    This is a frist Script

---
+++ firstscript.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# first script
+echo "Welcome to bash script"
+echo"---------------------------"
+echo "#############################################"
+echo "The uptime of the system is:-"
+uptime
+echo "##############################################"
+echo "Memory Utilization:-"
+free -m
+echo "##############################################"
+echo "Disk Utilization:-"
+df -h
+echo "##############################################"
+echo "End of the firstscript.................!!!"
