28ee748e6aaaedde8bdbdeff477ecdd615ef3f0a
Aditya Patil <98164681+itzaadiiiii@users.noreply.github.com>
2024-06-28 10:41:47 +0530

    Create Installing_aws_cli_ubuntu.sh
    Script for installing AWS-CLI on ubuntu machine

---
+++ Installing_aws_cli_ubuntu.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update
+sudo apt install -y curl unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+aws --version
